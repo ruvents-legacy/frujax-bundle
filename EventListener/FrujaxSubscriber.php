54882f5b867e107d0f60eb1d2cb57f8c444a5239
Valentin <udaltsov.valentin@gmail.com>
2017-07-19 21:43:58 +0300

    Response / Improvements

--- EventListener/FrujaxSubscriber.php
+++ EventListener/FrujaxSubscriber.php
@@ -36,11 +36,7 @@ class FrujaxSubscriber implements EventSubscriberInterface
             && $this->isFrujaxRequest($request)
             && $request->headers->has('Frujax-Intercept-Redirect')
         ) {
-            $frujaxResponse = new FrujaxRedirectResponse($response->getTargetUrl(), $response->headers->all());
-            $frujaxResponse->setProtocolVersion($response->getProtocolVersion());
-            $frujaxResponse->setCharset($response->getCharset());
-
-            $event->setResponse($frujaxResponse);
+            $event->setResponse(FrujaxRedirectResponse::fromRedirectResponse($response));
         }
     }
 
--- HttpFoundation/FrujaxRedirectResponse.php
+++ HttpFoundation/FrujaxRedirectResponse.php
@@ -2,6 +2,7 @@
 
 namespace Ruvents\FrujaxBundle\HttpFoundation;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class FrujaxRedirectResponse extends Response
@@ -13,11 +14,33 @@ class FrujaxRedirectResponse extends Response
 
     /**
      * {@inheritdoc}
+     *
+     * @param string $url
      */
-    public function __construct($url, $headers = [])
+    public function __construct($url, $status = 200, array $headers = [])
     {
-        parent::__construct('', 200, $headers);
+        parent::__construct('', $status, $headers);
         $this->setTargetUrl($url);
+    }
+
+    /**
+     * @param RedirectResponse $redirect
+     *
+     * @return static
+     */
+    public static function fromRedirectResponse(RedirectResponse $redirect)
+    {
+        $frujax = new static($redirect->getTargetUrl());
+
+        $frujax->setProtocolVersion($redirect->getProtocolVersion());
+        $frujax->setCharset($redirect->getCharset());
+        $frujax->headers = clone $redirect->headers;
+        $frujax->headers->remove('Location');
+
+        // we must set url again after replacing the headers
+        $frujax->setTargetUrl($redirect->getTargetUrl());
+
+        return $frujax;
     }
 
     /**
